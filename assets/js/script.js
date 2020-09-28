a00d94a7c589025e598a4c0b6e1efd258ee28df7
Melanie Bostwick <mbostwick1@gmail.com>
2020-09-28 15:30:34 -0400

    Added all text content to javascript file, start quiz frame

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// START QUIZ //
+
+function startQuiz() {
+  document.getElementById("headline").innerHTML = "Coding Quiz Challenge";
+  document.getElementById("pText").innerHTML =
+    "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+    document.getElementById("btn").innerHTML = "Start Quiz";
+
+    
+}
+
+startQuiz();
